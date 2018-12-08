79499bbd8233d41dea18fed12421faf9fe2be9cd
Paulo Goes <phsgoes@outlook.com>
2018-12-08 11:15:06 -0200

    Vuex installed. First store running with mutations

---
+++ src/components/ProductList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - {{ product.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import store from  '@/store'
+import shop from '@/api/shop'
+export default {
+  computed: {
+    products () {
+      return store.state.products;
+    }
+  },
+
+  created () {
+    shop.getProducts(products => store.commit('setProducts', products))
+  },
+
+  methods: {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
